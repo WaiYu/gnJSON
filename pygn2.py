e25ec7788ed45d235817dfac09b84e0ef76fcdaa
WaiYu Hsieh <waiyuhsieh@gmail.com>
2015-05-19 14:56:55 -0700

    Copy gnquery utility class from pygn

--- pygn2.py
+++ pygn2.py
@@ -94,3 +94,114 @@ def search(clientID='', userID='', artist='', album='', track='', toc=''):
     print responseXML
   
   return xml.dom.minidom.parse(response)
+
+class _gnquery:
+  """
+  A utility class for creating and configuring an XML query for POST'ing to
+  the Gracenote service
+  """
+
+  def __init__(self):
+    self.root = xml.etree.ElementTree.Element('QUERIES')
+    
+  def addAuth(self, clientID, userID):
+    auth = xml.etree.ElementTree.SubElement(self.root, 'AUTH')
+    client = xml.etree.ElementTree.SubElement(auth, 'CLIENT')
+    user = xml.etree.ElementTree.SubElement(auth, 'USER')
+  
+    client.text = clientID
+    user.text = userID
+  
+  def addQuery(self, cmd):
+    query = xml.etree.ElementTree.SubElement(self.root, 'QUERY')
+    query.attrib['CMD'] = cmd
+  
+  def addQueryMode(self, modeStr):
+    query = self.root.find('QUERY')
+    mode = xml.etree.ElementTree.SubElement(query, 'MODE')
+    mode.text = modeStr
+
+  def addQueryTextField(self, fieldName, value):
+    query = self.root.find('QUERY')
+    text = xml.etree.ElementTree.SubElement(query, 'TEXT')
+    text.attrib['TYPE'] = fieldName
+    text.text = value
+  
+  def addQueryOption(self, parameterName, value):
+    query = self.root.find('QUERY')
+    option = xml.etree.ElementTree.SubElement(query, 'OPTION')
+    parameter = xml.etree.ElementTree.SubElement(option, 'PARAMETER')
+    parameter.text = parameterName
+    valueElem = xml.etree.ElementTree.SubElement(option, 'VALUE')
+    valueElem.text = value
+  
+  def addQueryGNID(self, GNID):
+    query = self.root.find('QUERY')
+    GNIDElem = xml.etree.ElementTree.SubElement(query, 'GN_ID')
+    GNIDElem.text = GNID
+    
+  def addQueryClient(self, clientID):
+    query = self.root.find('QUERY')
+    client = xml.etree.ElementTree.SubElement(query, 'CLIENT')
+    client.text = clientID
+    
+  def addQueryRange(self, start, end):
+    query = self.root.find('QUERY')
+    queryRange = xml.etree.ElementTree.SubElement(query, 'RANGE')
+    rangeStart = xml.etree.ElementTree.SubElement(queryRange, 'START')
+    rangeStart.text = str(start)
+    rangeEnd = xml.etree.ElementTree.SubElement(queryRange, 'END')
+    rangeEnd.text = str(end)
+  
+  def addQueryTOC(self, toc):
+    # TOC is a string of format '150 20512 30837 50912 64107 78357 ...' 
+    query = self.root.find('QUERY')
+    tocElem = xml.etree.ElementTree.SubElement(query, 'TOC')
+    offsetElem = xml.etree.ElementTree.SubElement(tocElem, 'OFFSETS')
+    offsetElem.text = toc
+    
+  def toString(self):
+    return xml.etree.ElementTree.tostring(self.root)
+
+  #Methods added by Fabian to reflect the Rhythm use case
+
+  def addAttributeSeed(self, moodID, eraID, genreID):
+    query = self.root.find('QUERY')
+    seed = xml.etree.ElementTree.SubElement(query, 'SEED')
+    seed.attrib['TYPE'] = "ATTRIBUTE"
+    if genreID!='':
+      genreElement = xml.etree.ElementTree.SubElement(seed, 'GENRE')
+      genreElement.attrib['ID'] = genreID
+    if moodID!='':    
+      genreElement = xml.etree.ElementTree.SubElement(seed, 'MOOD')
+      genreElement.attrib['ID'] = moodID
+    if eraID!='':
+      genreElement = xml.etree.ElementTree.SubElement(seed, 'ERA')
+      genreElement.attrib['ID'] = eraID
+
+
+  def addTextSeed(self, artist, track):
+    query = self.root.find('QUERY')
+    seed = xml.etree.ElementTree.SubElement(query, 'SEED')
+    seed.attrib['TYPE'] = "TEXT"
+    if artist!='':
+      text = xml.etree.ElementTree.SubElement(seed, 'TEXT')
+      text.attrib['TYPE'] = "ARTIST"
+      text.text = artist
+    if track!='':
+      text = xml.etree.ElementTree.SubElement(seed, 'TEXT')
+      text.attrib['TYPE'] = "TRACK"
+      text.text = track
+  
+  def addQueryEVENT(self, eventType, gnID):
+    query = self.root.find('QUERY')
+    event = xml.etree.ElementTree.SubElement(query, 'EVENT')
+    event.attrib['TYPE'] = eventType
+    gnidTag = xml.etree.ElementTree.SubElement(event, 'GN_ID')
+    gnidTag.text = gnID
+
+  def addRadioID(self, radioID):
+    query = self.root.find('QUERY')
+    radio = xml.etree.ElementTree.SubElement(query, 'RADIO')
+    myradioid = xml.etree.ElementTree.SubElement(radio, 'ID')
+    myradioid.text = radioID
